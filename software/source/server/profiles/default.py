f68f83c6ba3702549be4df620f0eea0b09749094
Ben Xu <benx.xu@mail.utoronto.ca>
2024-12-30 15:26:36 -0500

    update local and default profile

--- software/source/server/profiles/default.py
+++ software/source/server/profiles/default.py
@@ -4,8 +4,8 @@ interpreter = Interpreter()
 interpreter.tts = "elevenlabs"
 interpreter.stt = "deepgram"
 
-interpreter.model = "claude-3-5-sonnet-20240620"
-interpreter.provider = "anthropic"
+interpreter.model = "gpt-4o"
+interpreter.provider = "openai"
 interpreter.auto_run = True
 
 interpreter.instructions = "Be very concise in your responses."
